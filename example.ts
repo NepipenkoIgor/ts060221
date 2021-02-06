28588594ac939c657f61ad89dea2a411121c5bbd
Igor Nepipenko <inepipenko@muellerone.com>
2021-02-06 12:13:40 +0200

    feat: start [ref:no-ref]

---
+++ example.ts
@@ -0,0 +1,32 @@
+Object.defineProperty(window, 'MySweetApp', {value: 'v1.0.0', writable: true});
+
+function deliveryMethod() {
+    // TODO
+    return 'overnight';
+}
+
+function shipWeight() {
+    const el = document.getElementById('weight');
+    if (!el) {
+        return 0;
+    }
+    return parseInt(el.innerHTML);
+}
+
+function sendUpdates(emailAddr: string | string[]) {
+    function sendEmail(addr: string) {
+        console.log(`Shipping to ${addr} via ${deliveryMethod() || 'standard'} delivery`);
+
+        if (shipWeight() > 100) {
+            console.log('WARNING: Oversize package');
+        }
+    }
+
+    if (Array.isArray(emailAddr)) {
+        emailAddr.forEach((val, _idx) => {
+            sendEmail(val.trim());
+        });
+    } else {
+        sendEmail(emailAddr.trim());
+    }
+}

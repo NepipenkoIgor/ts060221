6ec390719d234f3804a5dbeed606b3e0cee174a5
Igor Nepipenko <inepipenko@muellerone.com>
2021-02-07 14:14:00 +0200

    feat: functions [ref:no-ref]

---
+++ old/menu/index.ts
@@ -0,0 +1,24 @@
+import { generateMenu } from './menu';
+import { list } from './menu/data';
+
+$('.menu')
+    .html(generateMenu(list))
+    .on('click', (e: Event) => {
+        const el: HTMLElement = e.target as HTMLElement;
+        if (!el.classList.contains('title')) {
+            return;
+        }
+        const parentLi: HTMLLIElement = el.parentElement as HTMLLIElement;
+        parentLi.classList.toggle('menu-open');
+    })
+
+// const menuNav: HTMLDivElement = document.querySelector('.menu') as HTMLDivElement;
+// menuNav.innerHTML = generateMenu(list);
+// menuNav.addEventListener('click', (e: MouseEvent) => {
+//     const el: HTMLElement = e.target as HTMLElement;
+//     if (!el.classList.contains('title')) {
+//         return;
+//     }
+//     const parentLi: HTMLLIElement = el.parentElement as HTMLLIElement;
+//     parentLi.classList.toggle('menu-open');
+// })
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,92 @@
-import { generateMenu } from './menu';
-import { list } from './menu/data';
+// function average(a: number, b: number, c: number): string {
+//     const avg = (a + b + c) / 3;
+//     return `Average is ${avg}`;
+// }
+//
+// const v = average('2', 1);
+// const v1 = average(1, 1);
+// const v2: string = average(1, 1, 1);
 
-$('.menu')
-    .html(generateMenu(list))
-    .on('click', (e: Event) => {
-        const el: HTMLElement = e.target as HTMLElement;
-        if (!el.classList.contains('title')) {
-            return;
-        }
-        const parentLi: HTMLLIElement = el.parentElement as HTMLLIElement;
-        parentLi.classList.toggle('menu-open');
-    })
 
-// const menuNav: HTMLDivElement = document.querySelector('.menu') as HTMLDivElement;
-// menuNav.innerHTML = generateMenu(list);
-// menuNav.addEventListener('click', (e: MouseEvent) => {
-//     const el: HTMLElement = e.target as HTMLElement;
-//     if (!el.classList.contains('title')) {
-//         return;
+// function average(a: number, b?: number, c?: number): string {
+//     if (b === undefined) {
+//         b = 0;
 //     }
-//     const parentLi: HTMLLIElement = el.parentElement as HTMLLIElement;
-//     parentLi.classList.toggle('menu-open');
+//     if (c === undefined) {
+//         c = 0;
+//     }
+//     const avg = (a + b + c) / 3;
+//     return `Average is ${avg}`;
+// }
+//
+// const v = average('2', 1);
+// const v1 = average(1);
+// const v3 = average(1,2);
+// const v2: string = average(1, 1, 1);
+
+// function average(a: number, b: number = 0, c: number = 0): string {
+//     const avg = (a + b + c) / 3;
+//     return `Average is ${avg}`;
+// }
+//
+// const v = average('2', 1);
+// const v1 = average(1);
+// const v3 = average(1,2);
+// const v2: string = average(1, 1, 1);
+
+function isString(arg: string | number): arg is string {
+    if (typeof arg === 'string') {
+        return true;
+    }
+    return false;
+}
+type sn = string | number;
+function average(a: string , b: number): string;
+function average(a: number, s: string): string;
+function average(a: number, b: number, c: number): string;
+function average(...args: (string | number)[]): string {
+    let total = 0;
+    for (const arg of args) {
+        if (isString(arg)) {// string | number
+            total += Number(arg);
+            continue;
+        }
+        total += arg;
+    }
+    const avg = total / args.length;
+    return `Average is ${avg}`;
+}
+
+const v = average('2', 1);
+const v0 = average(1, 1);
+const v1 = average(1);
+const v3 = average(1, 2);
+const v2: string = average(1, 1, 1);
+const v5: string = average(1, 1, 1, 1, 2);
+
+// const arr: (string | number)[] = [1, '2', 2, '4'];
+// arr.filter((el: string | number): el is number => {
+//     return typeof el === 'number';
+// }).forEach((el) => {
+//     el.
 // })
+//
+// function isNumber(el: string | number): el is number {
+//     return typeof el === 'number';
+// }
+//
+//
+// let action: SomeAction1 | SomeAction2
+
+function getFullName(this: {name: string, surname: string}, age: number = 20, ..._args: number[]) {
+    return `${this.name} ${this.surname} ${age.toFixed()}`;
+}
+
+
+let account = {
+    name: 'Ihor',
+    surname: 'Nepipenko',
+    getFullName
+}
+
+account.getFullName();
